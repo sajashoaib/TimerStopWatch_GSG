617c1d0148f5ed2efa1453f3a6bb3647c49b3cf4
sajashoaib <islamsaja08@gmail.com>
2023-06-18 20:50:20 +0300

    add fuction with codes for stopwatch

--- main.js
+++ main.js
@@ -15,3 +15,49 @@ const stopwatchMSElem = document.getElementById('stopwatch-ms');
 const hourElem = document.getElementById('timer-hour');
 const minElem = document.getElementById('timer-min');
 const secElem = document.getElementById('timer-sec');
+
+function startStopwatch() {
+    stopwatchInterval = setInterval(updateStopwatch, 10);
+}
+
+function updateStopwatch() {
+    stopwatchMilliseconds++;
+    if (stopwatchMilliseconds === 100) {
+        stopwatchSeconds++;
+        stopwatchMilliseconds = 0;
+    }
+    if (stopwatchSeconds === 60) {
+        stopwatchMinutes++;
+        stopwatchSeconds = 0;
+    }
+    if (stopwatchMinutes === 60) {
+        stopwatchHours++;
+        stopwatchMinutes = 0;
+    }
+    displayStopwatch();
+}
+
+function pauseStopwatch() {
+    clearInterval(stopwatchInterval);
+}
+
+function resetStopwatch() {
+    clearInterval(stopwatchInterval);
+    stopwatchHours = 0;
+    stopwatchMinutes = 0;
+    stopwatchSeconds = 0;
+    stopwatchMilliseconds = 0;
+    displayStopwatch();
+}
+
+function displayStopwatch() {
+    stopwatchHourElem.textContent = formatTime(stopwatchHours);
+    stopwatchMinElem.textContent = formatTime(stopwatchMinutes);
+    stopwatchSecElem.textContent = formatTime(stopwatchSeconds);
+    stopwatchMSElem.textContent = formatTime(stopwatchMilliseconds);
+}
+
+// Helper function 
+function formatTime(time) {
+    return time.toString().padStart(2, '0');
+}
